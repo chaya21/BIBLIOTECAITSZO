00692e8506145aa8911d71efbc3bb73304ae7f71
chaya21 <monsrrat89@gmail.com>
2016-05-16 14:53:13 -0500

    Add files via upload

---
+++ Sistema/myapp/Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'n',
+		'database' => 'actividades_extra_escolares',
+	);
+}
